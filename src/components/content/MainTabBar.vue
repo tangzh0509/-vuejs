c683aa6b2d0ca57eeb4b550c8c0c2401c7f571f5
tangzh0509 <tangzh7836@gmail.com>
2020-09-30 15:51:39 +0800

    add some comfig files

---
+++ src/components/content/MainTabBar.vue
@@ -0,0 +1,43 @@
+<template>
+  <tabbar>
+    <tabbaritem path="/home">
+      <img slot="item-icon" src="~assets/img/home/home.svg">
+      <img slot="item-icon-active" src="~assets/img/home/home_active.svg">
+      <div slot="item-text">首页</div>
+    </tabbaritem>
+    <tabbaritem path="/discovery">
+      <img slot="item-icon" src="~assets/img/discovery/discovery.svg">
+      <img slot="item-icon-active" src="~assets/img/discovery/discovery_active.svg">
+      <div slot="item-text">发现</div>
+    </tabbaritem>
+    <tabbaritem path="/">
+      <div slot="item-text">+</div>
+    </tabbaritem>
+    <tabbaritem path="/shop">
+      <img slot="item-icon" src="~assets/img/shop/shop.svg">
+      <img slot="item-icon-active" src="~assets/img/shop/shop_active.svg">
+      <div slot="item-text">商店</div>
+    </tabbaritem>
+    <tabbaritem path="/profile">
+      <img slot="item-icon" src="~assets/img/profile/profile.svg">
+      <img slot="item-icon-active" src="~assets/img/profile/profile_active.svg">
+      <div slot="item-text">我的</div>
+    </tabbaritem>
+  </tabbar>
+</template>
+
+<script>
+import tabbar from '@/components/common/tabbar/TabBar.vue'
+import tabbaritem from '@/components/common/tabbar/TabBarItem.vue'
+export default {
+  name: "MainTabBar",
+  components:{
+    tabbar,
+    tabbaritem
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ vue.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    configureWebpack:{
+        resolve:{
+            alias:{
+                'assets':'@/assets',
+                'components':'@/components',
+                'router':'@/router',
+                'network':'@/network',
+                'views':'@/views'
+            }
+        }
+    }
+}
